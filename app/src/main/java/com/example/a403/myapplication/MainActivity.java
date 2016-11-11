54584c50a04a622348c383acaa5a1c9ce83a5705
parksungeun <tjddms134@nate.com>
2016-11-11 12:24:51 +0900

    picture

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.a403.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1;
+    Button b2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "안녕?", Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+
+        b2 = (Button) findViewById(R.id.button3);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "행복하다!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
